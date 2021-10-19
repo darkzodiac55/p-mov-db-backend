def4527ac5e817e645794ed94a725e10ef6b3147
Igor G <darkzodiac55@gmail.com>
2021-10-19 13:46:09 +0200

    middleware added

--- app.js
+++ app.js
@@ -9,13 +9,41 @@ const templates = [
     { order: 1, title: "Ghost in the Shell", url: "https://d3tvwjfge35btc.cloudfront.net/Assets/39/608/L_p0151960839.jpg", rating: "3", year: "1995" }
 ]
 
+const userMovs = [
+    
+]
+
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.get('/templates', (req, res) => {
+const logger = (req,res,next) =>{
+    const method = req.method
+    const url = req.url
+    const time = new Date()
+    let hours = time.getHours()
+    let minutes = time.getMinutes();
+    let month = ("0" + (time.getMonth() + 1)).slice(-2);
+    let date = ("0" + time.getDate()).slice(-2);
+    console.log(date + "-" + month + " " + hours + ":" + minutes);
+    next()
+
+}
+
+app.get('/templates', logger, (req, res) => {
     res.json(templates)
     
+})
+
+app.get('/usrmovs', (req, res) => {
+    res.json(userMovs)
+    
+})
+
+app.post('/nmov', (req,res) => {
+    console.log(req.body);
+    userMovs.push(req.body)
+    res.status(201).send({success:true})
 })
 
 app.listen(5000, () => {
