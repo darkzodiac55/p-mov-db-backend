6a5aa6923f9c087fe76f313eb4c6b328f79b0891
Igor G <darkzodiac55@gmail.com>
2021-10-21 10:41:42 +0200

    working with external files/REST routes

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const cors = require('cors')
 
+/* const fs = require('fs/promises');
+
+async function cr() {
+    await fs.writeFile('usermovs.json', 'cicik')
+} */
+
+
+const templates = require('./templates0.json')
 const app = express()
 
-const templates = [
+/* const templates = [
     { order: 1, title: "Forrest Gump", url: "https://m.media-amazon.com/images/I/31M9F+VrAWL._AC_.jpg", rating: "4", year: "1994" },
     { order: 1, title: "The Big Lebowski", url: "https://i.pinimg.com/564x/94/c0/25/94c02573dec058d69b22512abd21f1ba.jpg", rating: "5", year: "1998" },
     { order: 1, title: "Ghost in the Shell", url: "https://d3tvwjfge35btc.cloudfront.net/Assets/39/608/L_p0151960839.jpg", rating: "3", year: "1995" }
-]
+] */
+
+
 
 let userMovs = [
     
@@ -54,11 +64,15 @@ app.delete('/usrmovs/:id', (req, res) => {
     res.status(200).json({success:true})
 })
 
-app.post('/nmov', (req,res) => {
+app.post('/usrmovs', (req,res) => { /////restful route
     console.log(req.body);
-    userMovs.push(req.body)
+    const {nmov} = req.body ////destructure out for security
+    userMovs.push(nmov)   
     res.status(201).send({success:true})
 })
+
+
+
 
 app.listen(5000, () => {
     console.log('server listening on port 5000 :)');
